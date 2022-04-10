992575e5efbcf444ec0c082b7815ed4f083e93fc
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-10 14:35:40 +0300

    ft:added upvote and downvote buttons

--- src/app/upvote-downvote/upvote-downvote.component.ts
+++ src/app/upvote-downvote/upvote-downvote.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upvote-downvote.component.css']
 })
 export class UpvoteDownvoteComponent implements OnInit {
+  numberOfUpvotes: number= 0;
+  upvoteButtonClick(){
+    this.numberOfUpvotes++;
+
+  }
+
+  numberOfDownvotes:number= 0;
+  downvoteButtonClick(){
+    this.numberOfDownvotes++;
+  }
 
   constructor() { }
 
