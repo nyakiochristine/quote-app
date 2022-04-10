661585b6a754cb4daa7c46bba34e2e15c8e4ddce
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-10 11:02:34 +0300

    imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+
+import { UpvoteDownvoteComponent } from './upvote-downvote/upvote-downvote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuotesFormComponent,
+    UpvoteDownvoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+            
