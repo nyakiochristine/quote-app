5055fea716d295e306d42cbfd37d5790b8be2658
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-10 11:06:27 +0300

    initial commit

---
+++ src/app/upvote-downvote/upvote-downvote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upvote-downvote',
+  templateUrl: './upvote-downvote.component.html',
+  styleUrls: ['./upvote-downvote.component.css']
+})
+export class UpvoteDownvoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
