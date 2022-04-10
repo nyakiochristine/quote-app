5dd64b7623a0ec0ff5725ed2dfc1a141549814bf
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-10 17:24:12 +0300

    added consttructor implementation

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,11 +1,23 @@
 export class Quote {
-    showAuthor!:boolean;
+    showAuthor!: boolean;
     
-    public Id!: number;
-    public quo!: string;
-    public author!:string;
-    public completeDate!:Date;
+    constructor (
+    public id:Number,
+    public quo: string,
+    public author: string,
+    public completeDate: Date){
+        this.showAuthor= false;
+    }
 
     
 }
+
+    
+
+//export class Goal {
+    //public showDescription: boolean;
+    //constructor(public id: number,public name: string,public description: string, public completeDate: Date){
+    //  this.showDescription=false;
+    //}
+  //
 
