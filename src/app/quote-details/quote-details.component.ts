4121c07de6acb7d0234f05eb494d865356e2cc34
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-10 11:04:22 +0300

    linked it to upvote-downvote component

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input()
+  quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
   constructor() { }
 
