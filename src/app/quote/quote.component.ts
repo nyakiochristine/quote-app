b266fd97b5ac8f2e0016e57cbc69e21c5d8991b2
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-12 10:32:53 +0300

    update branch

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,86 @@
+ //import { Quote } from '@angular/compiler';
+import { Component, OnInit  } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+ 
+  quotes:Quote[]=[
+    new Quote(1,
+      'Monotony leads to poverty.Take a chance and eradicate it'
+      ,'Author:Andile',
+      new Date(2022,10,4)),
+
+      new Quote(2,
+        'Always Shine as Bright as Diamond. You are always meant to embrace your glory.',
+        'Author:Lucy'
+        ,
+        new Date(2022,10,4)),
+
+        new Quote(3,
+          'You are born to succeed in life. Affirm Yourself that you deserve it',
+          'Author:Njoki',
+          new Date(2022,10,4))
+  
+  ];
+
+  toggleDet(index:any){
+    this.quotes[index].showAuthor= !this.quotes[index].showAuthor
+  }
+
+  completeQuote(isComplete: any, index: any){
+    if(isComplete){
+      this.quotes.splice(index, 1)
+    }
+  }
+
+  deleteQuote(isComplete: any,index: any){
+    if (isComplete){
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index]}`)
+    
+    if(toDelete){
+      this.quotes.splice(index,1)
+    }
+   }
+  }
+  //new method//
+   addNewQuote(quote: any) {
+     let quoteLength=this.quotes.length;
+     quote.id= quoteLength +1;
+     quote.completeDate= new Date(quote.completeDate);
+     this.quotes.push(quote);
+
+     
+   }
+ 
+  
+      
+    
+ 
+    
+  
+
+
+
+
+  
+    
+  constructor(
+    
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
+function i(index: number, i: any) {
+  throw new Error('Function not implemented.');
+}
+
