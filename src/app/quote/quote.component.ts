19111b1476be4e2115202aceba59d34cfa364615
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-12 10:55:23 +0300

    patch:fixed errors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -4,7 +4,7 @@ import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
-  templateUrl: './quote.component.html',
+  templateUrl: './quote.component.html',                                                                                    
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
