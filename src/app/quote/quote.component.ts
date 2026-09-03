283cec8872c8e73526f75d835e4c22a1f6b3193c
Christine <christinemwangi368@gmail.com>
2026-09-03 09:43:48 +0300

    refactor: update styles and structure for quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -40,7 +40,7 @@ export class QuoteComponent implements OnInit {
 
   deleteQuote(isComplete: any,index: any){
     if (isComplete){
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index]}`)
+      let toDelete = confirm(`Are you sure you want to delete this quote?`)
     
     if(toDelete){
       this.quotes.splice(index,1)
